e84c985094a434265a2b886b022c7768adbd51a1
Carrie Kroutil <CarrieKroutil@gmail.com>
2019-01-27 12:40:17 -0600

    -Fixed app.yaml to use Go 11 runtime.
    -Fixed contact page to send email and update UI that action was successful.

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module kroutil.com/kona-coder/go-kona-doodle
+
+require google.golang.org/appengine v1.4.0
--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/mail"
 )
 
 var tpl *template.Template
@@ -16,11 +19,11 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
-type person struct {
-	FirstName  string
-	LastName   string
-	Email      string
-	Subscribed bool
+type subscriber struct {
+	Name    string
+	Phone   string
+	Email   string
+	Message string
 }
 
 func main() {
@@ -38,34 +41,72 @@ func main() {
 	http.Handle("/scripts/", http.StripPrefix("/scripts", http.FileServer(http.Dir("scripts"))))
 	http.Handle("/styles/", http.StripPrefix("/styles", http.FileServer(http.Dir("styles"))))
 
+	// This should NOT be needed since this is using the new go111 runtime but... to create a valid
+	// context object to use for sending mail via appengine... this is necessary. {facepalm}
+	// HOWEVER, if you run locally w/o commenting out below it was throw up at runtime.
+	appengine.Main()
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func index(res http.ResponseWriter, req *http.Request) {
+func index(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("index requested @ " + req.URL.Path)
 
 	if req.URL.Path != "/" {
-		http.NotFound(res, req)
+		http.NotFound(w, req)
 		return
 	}
 
-	err := tpl.ExecuteTemplate(res, "index.gohtml", nil)
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
 	if err != nil {
 		log.Fatalln("index template didn't execute: ", err)
 	}
 }
 
-func about(res http.ResponseWriter, req *http.Request) {
+func about(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("about requested")
-	err := tpl.ExecuteTemplate(res, "about.gohtml", nil)
+	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
 	if err != nil {
 		log.Fatalln("about template didn't execute: ", err)
 	}
 }
 
-func contact(res http.ResponseWriter, req *http.Request) {
+func contact(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("contact requested")
-	err := tpl.ExecuteTemplate(res, "contact.gohtml", nil)
+
+	n := req.FormValue("name")
+	p := req.FormValue("phone")
+	e := req.FormValue("email")
+	m := req.FormValue("message")
+
+	s := subscriber{
+		Name:    n,
+		Phone:   p,
+		Email:   e,
+		Message: m,
+	}
+
+	// Added check for > 50 in case the maxlength of the input is overridden by malicious user.
+	if e != "" || len(e) > 50 {
+		log.Println("Contact Email: ", e)
+		// Send email.
+		// GCP Doc: https://cloud.google.com/appengine/docs/standard/go/mail/sending-receiving-with-mail-api
+		ctx := appengine.NewContext(req)
+		req = req.WithContext(ctx)
+
+		msg := &mail.Message{
+			Sender: "carriekroutil@gmail.com", // FYI: Registered as an authorized sender in GCP so no password needed in code.
+			To: []string{"Carrie <carrie@shebytes.io>"},
+			Subject: "New Subscriber - Kona Doodle: " + e,
+			Body: "New Subscriber: \n\n Name: " + n + "\n Phone: " + p + "n Email: " + e + "\n\n Message: " + m,
+		}
+		if err := mail.Send(req.Context(), msg); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", s)
 	if err != nil {
 		log.Fatalln("contact template didn't execute: ", err)
 	}
@@ -119,12 +160,13 @@ func upload(w http.ResponseWriter, req *http.Request) {
 func subscribe(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("subscribe requested")
 
-	// body
-	bs := make([]byte, req.ContentLength)
-	req.Body.Read(bs)
-	body := string(bs)
+	e := req.FormValue("email")
 
-	err := tpl.ExecuteTemplate(w, "subscribe.gohtml", body)
+	if e != "" || len(e) > 50 {
+		log.Println("Email: ", e)
+	}
+
+	err := tpl.ExecuteTemplate(w, "subscribe.gohtml", e)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		log.Fatalln(err)
